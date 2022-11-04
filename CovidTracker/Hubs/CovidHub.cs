8bd8a5afdad3e2d5dcfc05e3ad6244531149e0db
Yiğit Can Ayaz <54958108+YigitCanAyaz@users.noreply.github.com>
2022-11-04 18:45:51 +0300

    Add project files.

---
+++ CovidTracker/Hubs/CovidHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace CovidTracker.Hubs
+{
+    public class CovidHub : Hub
+    {
+        public async Task GetCovidList()
+        {
+            await Clients.All.SendAsync("ReceiveCovidList", "take covid data from service");
+        }
+    }
+}
---
+++ CovidTracker/Models/CovidService.cs
@@ -0,0 +1,22 @@
+﻿using CovidTracker.Hubs;
+using Microsoft.AspNetCore.SignalR;
+
+namespace CovidTracker.Models
+{
+    public class CovidService
+    {
+        private readonly AppDbContext _context;
+        private readonly IHubContext<CovidHub> _hubContext;
+
+        public CovidService(AppDbContext context, IHubContext<CovidHub> hubContext)
+        {
+            _context = context;
+            _hubContext = hubContext;
+        }
+
+        public IQueryable<Covid> GetList()
+        {
+            return _context.Covids.AsQueryable();
+        }
+    }
+}
